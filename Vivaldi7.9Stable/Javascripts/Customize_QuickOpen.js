0d15c2d793599638b24c8f277b324c4e9ea1eb78
Hanata Gaori <78333341+doctorgreen4721@users.noreply.github.com>
2026-04-27 00:20:26 +0800

    Update Customize_QuickOpen.js

--- Vivaldi7.9Stable/Javascripts/Customize_QuickOpen.js
+++ Vivaldi7.9Stable/Javascripts/Customize_QuickOpen.js
@@ -27,6 +27,11 @@
         '<path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.4 5.4 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65S8.93 17.38 9 18v4"/>' +
         '<path d="M9 18c-4.51 2-5-2-7-2"/></svg>';
 
+    var SVG_RESTART =
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">' +
+        '<path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/>' +
+        '<path d="M3 3v5h5"/></svg>';
+
     // ========== Constants ==========
     var name = '\u5feb\u6377\u6253\u5f00';
     var webPanelId = 'WEBPANEL_quick-opena1b2c3d4e';
@@ -122,6 +127,115 @@
         });
     }
 
+    // ========== Restart Logic ==========
+    function setInputValue(input, value) {
+        var descriptor = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value');
+        if (descriptor && descriptor.set) {
+            descriptor.set.call(input, value);
+        } else {
+            input.value = value;
+        }
+    }
+
+    function submitUrlThroughAddressBar(url) {
+        var input = document.querySelector('#urlFieldInput');
+        if (!input) return false;
+
+        try {
+            input.focus();
+            if (typeof input.select === 'function') {
+                input.select();
+            }
+            setInputValue(input, url);
+            input.dispatchEvent(new Event('input', { bubbles: true }));
+            input.dispatchEvent(new Event('change', { bubbles: true }));
+
+            var eventInit = {
+                key: 'Enter',
+                code: 'Enter',
+                keyCode: 13,
+                which: 13,
+                bubbles: true,
+                cancelable: true,
+                composed: true,
+            };
+
+            input.dispatchEvent(new KeyboardEvent('keydown', eventInit));
+            input.dispatchEvent(new KeyboardEvent('keypress', eventInit));
+            input.dispatchEvent(new KeyboardEvent('keyup', eventInit));
+            return true;
+        } catch (e) { /* ignore */ }
+
+        return false;
+    }
+
+    function openRestartTab(url) {
+        try {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                if (tabs && tabs[0]) {
+                    chrome.tabs.update(tabs[0].id, { url: url, active: true }, function () {
+                        if (chrome.runtime.lastError) {
+                            try {
+                                chrome.tabs.create({ url: url, active: true });
+                            } catch (e) { /* ignore */ }
+                        }
+                    });
+                    return;
+                }
+
+                chrome.tabs.create({ url: url, active: true });
+            });
+            return true;
+        } catch (e) {
+            try {
+                chrome.tabs.create({ url: url, active: true });
+                return true;
+            } catch (err) { /* ignore */ }
+        }
+
+        return false;
+    }
+
+    function doRestart() {
+        var restartHandled = false;
+
+        function markRestartHandled() {
+            restartHandled = true;
+        }
+
+        window.addEventListener('beforeunload', markRestartHandled, { once: true });
+
+        try {
+            if (window.vivaldi && vivaldi.utilities && typeof vivaldi.utilities.restart === 'function') {
+                vivaldi.utilities.restart();
+            }
+        } catch (e) { /* ignore */ }
+
+        setTimeout(function () {
+            if (restartHandled) return;
+
+            if (submitUrlThroughAddressBar('vivaldi://restart/')) {
+                setTimeout(function () {
+                    if (!restartHandled) {
+                        submitUrlThroughAddressBar('chrome://restart/');
+                    }
+                }, 200);
+            }
+        }, 120);
+
+        setTimeout(function () {
+            if (!restartHandled) {
+                openRestartTab('vivaldi://restart/');
+            }
+        }, 420);
+
+        setTimeout(function () {
+            if (!restartHandled) {
+                openRestartTab('chrome://restart/');
+            }
+        }, 720);
+    }
+
     // ========== Panel UI (renders inside the sidebar panel area) ==========
     var panelRoot = null;
 
@@ -132,6 +246,7 @@
         var items = [
             { url: 'vivaldi://extensions', label: '\u6269\u5c55\u7ba1\u7406', svg: SVG_LAYERS },
             { url: 'vivaldi://inspect/#apps', label: '\u524d\u7aef\u8bca\u65ad', svg: SVG_DEV },
+            { label: '\u91cd\u542f Vivaldi', svg: SVG_RESTART, action: 'restart' },
         ];
 
         items.forEach(function (item) {
@@ -151,7 +266,11 @@
             panelRoot.appendChild(div);
 
             div.addEventListener('click', function () {
-                chrome.tabs.create({ url: item.url });
+                if (item.action === 'restart') {
+                    doRestart();
+                } else {
+                    chrome.tabs.create({ url: item.url });
+                }
             });
         });
 
